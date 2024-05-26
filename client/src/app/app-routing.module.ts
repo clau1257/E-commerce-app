6e4a705389cb6a576a2b17fc39b198969672ee19
Claudiu <clau1257@yahoo.com>
2024-05-27 02:58:47 +0300

    update 2 basket

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,9 @@ const routes: Routes = [
   {path: 'not-found', component : NotFoundComponent},
   {path: 'server-error', component : ServerErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module')
+  .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
-export class ProductItemComponent {
-  @Input() product?: Product;
+export class ProductItemComponent implements OnInit {
+  @Input() product: Product;
+
+  constructor(private basketService: BasketService) { }
+
+  ngOnInit() {
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product);
+  }
 
 }
